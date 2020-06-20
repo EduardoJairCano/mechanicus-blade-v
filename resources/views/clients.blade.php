e783177e6fb99e174f2873de84e75fc768a1ad47
Eduardo Jair Cano <jair.ci@incfile.com>
2020-06-20 13:30:15 -0500

    First blade directives

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -1,11 +1,7 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Clientes </title>
-</head>
-<body>
+@extends('layout')
+
+@section('title', 'Clientes')
+
+@section('content')
     <h1> Clientes </h1>
-</body>
-</html>
+@endsection
