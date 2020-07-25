f758c77eb8d2bb6b306dca857ceeea4cf42712a5
Eduardo Jair Cano <jair.ci@incfile.com>
2020-07-25 00:37:27 -0500

    Add Route::resource() for customer actions

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,9 @@ Route::get('/home/{username?}', static function ($username = 'usuario') {
     ]);
 })->name('home');
 
-Route::get('/clientes', 'CustomerController@index')->name('customers.index');
-Route::get('/clientes/crear', 'CustomerController@create')->name('customers.create');
-
-Route::get('/clientes/{customer}/editar', 'CustomerController@edit')->name('customers.edit');
-Route::patch('/clientes/{customer}', 'CustomerController@update')->name('customers.update');
-
-Route::post('/clientes', 'CustomerController@store')->name('customers.store');
-Route::get('/clientes/{customer}', 'CustomerController@show')->name('customers.show');
-
-Route::delete('/clientes/{customer}', 'CustomerController@destroy')->name('customers.destroy');
+Route::resource('clientes', 'CustomerController')
+    ->parameters(['clientes' => 'customer'])
+    ->names('customers');
 
 Route::post('customers', 'MessageController@store')->name('messages.store');
 
