359463deed00ed63177dc56cd625d0c67f0340ce
Eduardo Jair Cano <jair.ci@incfile.com>
2020-07-24 23:58:17 -0500

    Create partial/validation-errors blade

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -5,13 +5,7 @@
 @section('content')
     <h1> Agregar nuevo cliente </h1>
 
-    @if($errors->any())
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+    @include('partials.validation-errors')
 
     <form method="POST" action="{{ route('customers.store') }}">
         @csrf
--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -5,13 +5,7 @@
 @section('content')
     <h1> Editar cliente </h1>
 
-    @if($errors->any())
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+    @include('partials.validation-errors')
 
     <form method="POST" action="{{ route('customers.update', $customer) }}">
         @csrf @method('PATCH')
