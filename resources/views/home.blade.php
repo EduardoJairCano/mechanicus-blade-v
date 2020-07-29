a704e82798651ce48a12046e2d74dfd3d8556a9e
Eduardo Jair Cano <jair.ci@incfile.com>
2020-07-29 10:29:56 -0500

    Add Navigation card on Home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header"> Navegación </div>
+
+                <div class="card-body">
+                    <a href="{{ route('customers.index') }}">Clientes</a><br>
+                    <a href="{{ route('about') }}">Acerca de</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-9">
             <div class="card">
                 <div class="card-header"> Panel de usuario </div>
 
@@ -19,6 +31,7 @@
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
