e5ed63a966768563a1fcc4a113c0f6a64e7b90f1
Eduardo Jair Cano <jair.ci@incfile.com>
2020-08-01 13:51:16 -0500

    Add npm dependences and webpack file modification

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,17 @@
 const mix = require('laravel-mix');
+/*
+ |--------------------------------------------------------------------------
+ | Laravel Mix
+ |--------------------------------------------------------------------------
+ |
+ | Laravel Mix provides a fluent API for defining Webpack build steps for
+ | your Laravel application using several common CSS and JavaScript
+ | pre-processors.
+ |
+ | Documentation
+ | - https://laravel.com/docs/7.x/mix
+ */
+
 
 /*
  |--------------------------------------------------------------------------
@@ -13,3 +26,32 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Browser Sync
+ |--------------------------------------------------------------------------
+ |
+ | Mix BrowserSync will automatically monitor your files for changes, and
+ | insert your changes into the browser - all without requiring a manual
+ | refresh.
+ |
+ | - https://laravel-mix.com/docs/5.0/browsersync
+ */
+mix.browserSync('http://mechanicus.test');
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Versioning
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides an ID version to identify whenever the file will be modified.
+ | This only will be applied to a production environment.
+ |
+ | - https://laravel-mix.com/docs/5.0/versioning
+ */
+if (mix.inProduction()) {
+    mix.version();
+}
