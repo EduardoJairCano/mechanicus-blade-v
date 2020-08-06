09d8951ac479587662398a53b32109bfc931f488
Eduardo Jair Cano <jair.ci@incfile.com>
2020-08-06 16:59:48 -0500

    Add UserInfoController and SaveUSerInfoRequest files

---
+++ app/Http/Controllers/UserInfoController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SaveUserInfoRequest;
+use App\Models\Address;
+use App\Models\UserInfo;
+use App\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class UserInfoController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|Response|View
+     */
+    public function index()
+    {
+        $user = Auth::user();
+
+        return view('userInfo.index', compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Application|Factory|Response|View
+     */
+    public function create()
+    {
+        //
+        return view('userInfo.create', [
+            'userInfo' => new UserInfo(),
+        ]);
+    }
+
+    /**
+     * Store a newly created user_info in storage.
+     *
+     * @param SaveUserInfoRequest $request
+     * @return Application|Factory|View
+     */
+    public function store(SaveUserInfoRequest $request)
+    {
+        $user = Auth::user();
+
+        if ($user) {
+            // Fields validations
+            $fields = $request->validated();
+
+            // Create new user_info row
+            UserInfo::create([
+                'first_name'        => $fields['first_name'],
+                'last_name'         => $fields['last_name'],
+                'rfc'               => $fields['rfc'],
+                'cell_phone_number' => $fields['cell_phone_number'],
+                'role'              => 1,
+                'user_id'           => $user->id,
+            ]);
+
+            // Create new address row
+            Address::create([
+                'street_address'    => $fields['street_address'],
+                'outdoor_number'    => $fields['outdoor_number'],
+                'interior_number'   => $fields['interior_number'],
+                'colony'            => $fields['colony'],
+                'postal_code'       => $fields['postal_code'],
+                'city'              => $fields['municipality'],
+                'municipality'      => $fields['municipality'],
+                'state'             => $fields['state'],
+                'country'           => $fields['country'],
+                'phone_number'      => $fields['phone_number'],
+                'fax_number'        => $fields['fax_number'],
+                'addressable_id'    => $user->id,
+                'addressable_type'  => User::class,
+            ]);
+        }
+
+        // ToDo: Successfully message
+
+        return view('home');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/SaveUserInfoRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SaveUserInfoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            /* User info */
+            'first_name'        => 'required',
+            'last_name'         => 'required',
+            'rfc'               => '',
+            'cell_phone_number' => 'required|numeric',
+            /* Address */
+            'street_address'    => 'required',
+            'outdoor_number'    => 'required|numeric',
+            'interior_number'   => '',
+            'colony'            => 'required',
+            'postal_code'       => 'required|numeric',
+            'municipality'      => 'required',
+            'state'             => 'required',
+            'country'           => 'required',
+            'phone_number'      => '',
+            'fax_number'        => '',
+        ];
+    }
+
+    /**
+     * Get the specific messages for errors.
+     *
+     * @return array|string[]
+     */
+    public function messages(): array
+    {
+        return [
+            /*
+            // User info
+            'first_name.required'           => 'Ingrese Nombre(s) del usuario',
+            'last_name.required'            => 'Ingrese Apellido(s) del usuario',
+            'cell_phone_number.required'    => 'Ingrese el Teléfono Móvil del usario',
+            'cell_phone_number.numeric'     => 'El valor debe ser numérico',
+            // Address
+            'street_address.required'       => 'Ingrese nombre de la Calle',
+            'outdoor_number.required'       => 'Ingrese Número Exterior',
+            'outdoor_number.numeric'        => 'El valor debe ser numérico',
+            'colony.required'               => 'Ingrese nombre de la Colonia',
+            'postal_code.required'          => 'Ingrese Código Postal',
+            'postal_code.numeric'           => 'El valor debe ser numérico',
+            'municipality.required'         => 'Ingrese nombre del Municipio o Ciudad',
+            'state.required'                => 'Ingrese nombre del Estado',
+            'country.required'              => 'Ingrese nombre del País',
+            */
+        ];
+    }
+}
