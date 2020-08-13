caafc2a58a0bf9367c6a95c47ad471af5151a322
Eduardo Jair Cano <jair.ci@incfile.com>
2020-08-12 23:46:04 -0500

    New Role model file

---
+++ app/Models/Role.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    /**
+     * The table name that belongs this model.
+     *
+     * @var string
+     */
+    protected $table = 'roles';
+
+
+}
