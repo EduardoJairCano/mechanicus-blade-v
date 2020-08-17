31b54bfd6e80b94394051b1c3dfa3cd1d9c53c99
Eduardo Jair Cano <jair.ci@incfile.com>
2020-08-17 10:45:27 -0500

    Add Edit&Delete buttons in list

--- resources/views/administrators/list.blade.php
+++ resources/views/administrators/list.blade.php
@@ -26,19 +26,34 @@
                 </tr>
             </thead>
             <tbody>
+            @foreach($subUsers as $subUser)
                 <tr>
-                    @foreach($subUsers as $subUser)
-                        <td>
-                            <a href="{{ route('administrator.show', $subUser) }}">
-                                {!! $subUser->userInfo->first_name . ' ' . $subUser->userInfo->last_name !!}
+                    <td>
+                        <a href="{{ route('administrator.show', $subUser) }}">
+                            {!! $subUser->userInfo->first_name . ' ' . $subUser->userInfo->last_name !!}
+                        </a>
+                    </td>
+                    <td>{{ $subUser->userInfo->cell_phone_number }}</td>
+                    <td>{{ $subUser->email }}</td>
+                    <td>{{ $subUser->role->display_name }}</td>
+                    <td>
+                        <div class="btn-group-sm" role="group">
+                            <a href="{{ route('administrator.edit', $subUser) }}" class="btn btn-primary">
+                                <i class="fas fa-user-edit"></i>
                             </a>
-                        </td>
-                        <td>{{ $subUser->userInfo->cell_phone_number }}</td>
-                        <td>{{ $subUser->email }}</td>
-                        <td>{{ $subUser->role->display_name }}</td>
-                        <td>btn y btn </td>
-                    @endforeach
+                            <a href="#" onclick="document.getElementById('delete-administrator').submit()" class="btn btn-danger">
+                                <i class="fa fa-close"></i>
+                            </a>
+                            <form id="delete-administrator"
+                                  method="POST"
+                                  action="{{ route('administrator.destroy', $subUser) }}"
+                                  class="d-none">
+                                @csrf @method('DELETE')
+                            </form>
+                        </div>
+                    </td>
                 </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
