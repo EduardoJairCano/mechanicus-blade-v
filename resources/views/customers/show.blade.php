a14c486a68ccc06ca8d8697d195c04b96eb81ff1
Eduardo Jair Cano <jair.ci@incfile.com>
2020-08-19 22:56:27 -0500

    Add new customers/partials section

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -5,6 +5,7 @@
 @section('card-title', 'Información del Cliente')
 
 @section('content')
+    {{-- Header & Action section --}}
     <div class="row align-items-center">
         <div class="col-md-6 offset-1">
             <div class="row col-md-12">
@@ -40,104 +41,8 @@
 
     <hr>
 
-    <div class="row">
-        <div class="col-md-4">
-            <span class="d-flex justify-content-end font-weight-bold">
-                RFC
-            </span>
-        </div>
-        <div class="col-md-7">
-            {{ $customer->rfc }}
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <span class="d-flex justify-content-end font-weight-bold">
-                Número de Móvil
-            </span>
-        </div>
-        <div class="col-md-7">
-            {{ $customer->cell_phone_number }}
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <span class="d-flex justify-content-end font-weight-bold">
-                Número de Casa
-            </span>
-        </div>
-        <div class="col-md-7">
-            {{ $customer->address->phone_number }}
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <span class="d-flex justify-content-end font-weight-bold">
-                Domicilio
-            </span>
-        </div>
-        <div class="col-md-7">
-            {!! $customer->address->street_address . ' ' . $customer->address->outdoor_number !!}
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <span class="d-flex justify-content-end font-weight-bold">
-                Colonia
-            </span>
-        </div>
-        <div class="col-md-7">
-            {{ $customer->address->colony }}
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <span class="d-flex justify-content-end font-weight-bold">
-                Código Postal
-            </span>
-        </div>
-        <div class="col-md-7">
-            {{ $customer->address->postal_code }}
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <span class="d-flex justify-content-end font-weight-bold">
-                Ciudad
-            </span>
-        </div>
-        <div class="col-md-7">
-            {{ $customer->address->city }}
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <span class="d-flex justify-content-end font-weight-bold">
-                Estado
-            </span>
-        </div>
-        <div class="col-md-7">
-            {{ $customer->address->state }}
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <span class="d-flex justify-content-end font-weight-bold">
-                País
-            </span>
-        </div>
-        <div class="col-md-7">
-            {{ $customer->address->country }}
-        </div>
-    </div>
+    {{-- Customer info section --}}
+    @include('customers.partials.show_customer_info', $customer)
 
     <div class="row">
         <div class="col-md-1 offset-10">
